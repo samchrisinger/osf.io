3b00d8bc94f47ead5db5f21e9845b4e415d717f7
Sam Chrisinger <s.chrisinger@gmail.com>
2015-09-30 09:57:33 -0400

    Move UserSerializer's profile_image_url to /links/profile_url

--- api/users/serializers.py
+++ api/users/serializers.py
@@ -5,6 +5,7 @@ from website.models import User
 from api.base.serializers import (
     JSONAPISerializer, LinksField, JSONAPIHyperlinkedIdentityField, DevOnly, IDField, TypeField
 )
+from api.base.utils import add_dev_only_items
 
 
 class UserSerializer(JSONAPISerializer):
@@ -24,31 +25,23 @@ class UserSerializer(JSONAPISerializer):
     suffix = ser.CharField(required=False, allow_blank=True, help_text='For bibliographic citations')
     date_registered = ser.DateTimeField(read_only=True)
 
-    profile_image_url = DevOnly(ser.SerializerMethodField(required=False, read_only=True))
-
-    def get_profile_image_url(self, user):
-        size = self.context['request'].query_params.get('profile_image_size')
-        return user.profile_image_url(size=size)
-
     # Social Fields are broken out to get around DRF complex object bug and to make API updating more user friendly.
-    gitHub = DevOnly(AllowMissing(ser.CharField(required=False, source='social.github',
-                                                allow_blank=True, help_text='GitHub Handle'), required=False, source='social.github'))
-    scholar = DevOnly(AllowMissing(ser.CharField(required=False, source='social.scholar',
-                                                 allow_blank=True, help_text='Google Scholar Account'), required=False, source='social.scholar'))
-    personal_website = DevOnly(AllowMissing(ser.URLField(required=False, source='social.personal',
-                                                         allow_blank=True, help_text='Personal Website'), required=False, source='social.personal'))
-    twitter = DevOnly(AllowMissing(ser.CharField(required=False, source='social.twitter',
-                                                 allow_blank=True, help_text='Twitter Handle'), required=False, source='social.twitter'))
-    linkedIn = DevOnly(AllowMissing(ser.CharField(required=False, source='social.linkedIn',
-                                                  allow_blank=True, help_text='LinkedIn Account'), required=False, source='social.linkedIn'))
-    impactStory = DevOnly(AllowMissing(ser.CharField(required=False, source='social.impactStory',
-                                                     allow_blank=True, help_text='ImpactStory Account'), required=False, source='social.impactStory'))
-    orcid = DevOnly(AllowMissing(ser.CharField(required=False, source='social.orcid',
-                                               allow_blank=True, help_text='ORCID'), required=False, source='social.orcid'))
-    researcherId = DevOnly(AllowMissing(ser.CharField(required=False, source='social.researcherId',
-                                                      allow_blank=True, help_text='ResearcherId Account'), required=False, source='social.researcherId'))
+    gitHub = DevOnly(ser.CharField(required=False, label='GitHub', source='social.github', allow_blank=True, help_text='GitHub Handle'))
+    scholar = DevOnly(ser.CharField(required=False, source='social.scholar', allow_blank=True, help_text='Google Scholar Account'))
+    personal_website = DevOnly(ser.URLField(required=False, source='social.personal', allow_blank=True, help_text='Personal Website'))
+    twitter = DevOnly(ser.CharField(required=False, source='social.twitter', allow_blank=True, help_text='Twitter Handle'))
+    linkedIn = DevOnly(ser.CharField(required=False, source='social.linkedIn', allow_blank=True, help_text='LinkedIn Account'))
+    impactStory = DevOnly(ser.CharField(required=False, source='social.impactStory', allow_blank=True, help_text='ImpactStory Account'))
+    orcid = DevOnly(ser.CharField(required=False, label='ORCID', source='social.orcid', allow_blank=True, help_text='ORCID'))
+    researcherId = DevOnly(ser.CharField(required=False, label='ResearcherID', source='social.researcherId', allow_blank=True, help_text='ResearcherId Account'))
 
-    links = LinksField({'html': 'absolute_url'})
+    links = LinksField(
+        add_dev_only_items({
+            'html': 'absolute_url',
+        }, {
+            'profile_image': 'profile_image_url',
+        })
+    )
     nodes = JSONAPIHyperlinkedIdentityField(view_name='users:user-nodes', lookup_field='pk', lookup_url_kwarg='user_id',
                                              link_type='related')
 
@@ -57,6 +50,10 @@ class UserSerializer(JSONAPISerializer):
 
     def absolute_url(self, obj):
         return obj.absolute_url
+
+    def profile_image_url(self, user):
+        size = self.context['request'].query_params.get('profile_image_size')
+        return user.profile_image_url(size=size)
 
     def update(self, instance, validated_data):
         assert isinstance(instance, User), 'instance must be a User'
